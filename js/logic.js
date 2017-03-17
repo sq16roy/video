17c0a5bf53dc4ab6a92daef1beb40cb651419d26
¨roy <sq16roy@gmail.com>
2017-03-17 15:52:25 -0600

    falta la imagen de error

--- js/logic.js
+++ js/logic.js
@@ -1,81 +1,129 @@
-
-    (function(){
-        initialisemyPlayer();
-    })();
-    // Variables to store handles to various required elements
-    var myPlayer;
-    var playPauseBtn;
-    var muteBtn;
-    var progressBar;
-    var myPlayer;
-    function initialisemyPlayer() {
+(function() {
+    launcher();
+})();
+// Variables to store handles to various required elements
+var myPlayer,
+    playPauseBtn,
+    muteBtn,
+    progressBar,
+    myPlayer,
+    seekBar,
+    seekVolume,
+    tempVol = seekVolume.value;
+//main function
+function launcher() {
     myPlayer = document.getElementById('media-video');
+    seekBar = document.getElementById('progress-bar');
+    seekVolume = document.getElementById('volume-bar');
     myPlayer.controls = false;
     myPlayer.addEventListener('timeupdate', updateProgressBar, false);
-    }
-    function togglePlayPause() {
+    seekBar.addEventListener('change', lookBar, false);
+    seekVolume.addEventListener('change', changeVolume, false);
+}
+
+//function to look for changes in the progress bar
+function lookBar(){
+    let getBar = myPlayer.duration*(seekBar.value/100);
+    myPlayer.currentTime = getBar;
+};
+//play pause function
+function togglePlayPause() {
     var btn = document.getElementById('play-pause-button');
     if (myPlayer.paused || myPlayer.ended) {
         btn.className = 'fa fa-pause-circle-o';
+        document.getElementById('click_on_button').className = 'fa fa-play-circle-o hide-me';
         myPlayer.play();
-    }
-    else {
+    } else {
         btn.className = 'fa fa-play-circle-o';
+        document.getElementById('click_on_button').className = 'fa fa-pause-circle-o show-me';
         myPlayer.pause();
     }
 }
+//stop function 
 function stopPlayer() {
-   myPlayer.pause();
-   myPlayer.currentTime = 0;
-   document.getElementById('play-pause-button').className = 'fa fa-play-circle-o';
+    myPlayer.pause();
+    myPlayer.currentTime = 0;
+    document.getElementById('play-pause-button').className = 'fa fa-play-circle-o';
+    document.getElementById('click_on_button').className = 'fa fa-play-circle-o show-me';
 }
-function changeVolume(direction) {
-   if (direction === '+') myPlayer.volume += myPlayer.volume == 1 ? 0 : 0.1;
-   else myPlayer.volume -= (myPlayer.volume == 0 ? 0 : 0.1);
-   myPlayer.volume = parseFloat(myPlayer.volume).toFixed(1);
-}
+//function change volume
+function changeVolume() {
+    var btn = document.getElementById('mute-button');
+    myPlayer.volume = seekVolume.value;
+    if (seekVolume.value > 0) {
+        tempVol = seekVolume.value;
+    }
+    if (seekVolume.value == 0) {
+        changeButtonType(btn, myIcon());
+        myPlayer.muted = true;
+    } else {
+        changeButtonType(btn, myIcon());
+        myPlayer.muted = false;
+    }
+};
+
+//function to return the icon
+function myIcon(){
+    if (seekVolume.value == 0.5) {
+        return 'fa fa-volume-down';
+    } else if(seekVolume.value >0.5){
+        return 'fa fa-volume-up';
+    } else if(seekVolume.value < 0.1){
+        return 'fa fa-volume-off';
+    } else if(seekVolume.value >0 && seekVolume.value < 0.5){
+        return 'fa fa-volume-down';
+    }
+};
+//function mute
 function toggleMute() {
-   var btn = document.getElementById('mute-button');
-   if (myPlayer.muted) {
-      changeButtonType(btn, 'fa fa-volume-off');
-      myPlayer.muted = false;
-   }
-   else {
-      changeButtonType(btn, 'fa fa-volume-up');
-      myPlayer.muted = true;
-   }
-}
-function replayMedia() {
-   resetPlayer();
-   myPlayer.play();
-}
-function resetPlayer() {
-   progressBar.value = 0;
-   myPlayer.currentTime = 0;
-   changeButtonType(playPauseBtn, 'play');
-}
-function changeButtonType(btn, value) {
-   btn.className = value;
-}
-var secondTimer =0;
-function updateProgressBar() {
-   let minutesTimer =0;
-   
-        if (secondTimer >60) {
-            secondTimer =0;
-        } else{
-             setTimeout(function(){secondTimer++;}, 5000);
+    var btn = document.getElementById('mute-button');
+    if (myPlayer.muted) {
+        changeButtonType(btn, myIcon());    
+        myPlayer.muted = false;
+        if (seekVolume.value == 0) {
+            myPlayer.volume = tempVol;
+            seekVolume.value = tempVol;
+            changeButtonType(btn, myIcon());
+        } else {
+            changeButtonType(btn, myIcon());
+            myPlayer.volume = tempVol;
+            seekVolume.value = tempVol; 
         }
-       minutesTimer = Math.floor(myPlayer.currentTime) /60;
-       console.log(secondTimer);
-       document.getElementById('minutes').innerHTML = "time:"+"m:"+Math.floor(minutesTimer)+"s:"+secondTimer;
-   var progressBar = document.getElementById('progress-bar');
-   var percentage = Math.floor((100 / myPlayer.duration) *
-   myPlayer.currentTime);
-  // console.log(Math.floor(myPlayer.currentTime));
-   progressBar.value = percentage;
-   progressBar.innerHTML = percentage + '% played';
-   if (percentage === 100) {
-       document.getElementById('play-pause-button').className = 'fa fa-repeat';
-   }
+    } else {
+        changeButtonType(btn, 'fa fa-volume-off');
+        myPlayer.muted = true;
+        seekVolume.value = 0;
+    }
+    console.log(myPlayer.muted+" "+seekVolume.value+" "+tempVol);
+}
+
+function resetPlayer() {
+    progressBar.value = 0;
+    myPlayer.currentTime = 0;
+}
+//function to change icon values
+function changeButtonType(btn, value) {
+    btn.className = value;
+}
+
+//function to update the progress
+function updateProgressBar() {
+    let minutesTimer = Math.floor(myPlayer.currentTime / 60);
+    let secondTimer = Math.floor(myPlayer.currentTime - minutesTimer * 60);
+    let durationTimer = Math.floor(myPlayer.duration / 60);
+    let secDurrationTimer = Math.floor(myPlayer.duration - durationTimer * 60);
+    let hourTimer = 0;
+    if (secondTimer < 10) {
+        secondTimer = "0"+secondTimer;
+    }
+    if (minutesTimer < 10) {
+        minutesTimer = "0"+minutesTimer;
+    }
+    document.getElementById('minutes').innerHTML = minutesTimer + ":" + secondTimer+"/"+durationTimer + ":" + secDurrationTimer;
+    var progressBar = document.getElementById('progress-bar');
+    var percentage = Math.floor((100 / myPlayer.duration) *myPlayer.currentTime);
+    progressBar.value = percentage;
+    if (percentage === 100) {
+        document.getElementById('play-pause-button').className = 'fa fa-repeat';
+    }
 }
